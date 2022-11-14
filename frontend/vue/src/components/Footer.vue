9227b70d29b451d12c8b536344b0a4ff780e0fa7
MUSHLIHUN <muslihun@untan.ac.id>
2022-11-14 11:51:36 +0700

    added frontend

---
+++ frontend/vue/src/components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer class="app-footer">
+    <span><a href="https://twitter.com/Cmiltone" target="_blank">@cmiltone</a> &copy; 2018 Castro.</span>
+    <span class="ml-auto">Powered by <a href="http://coreui.io">CoreUI</a></span>
+  </footer>
+</template>
+<script>
+export default {
+  name: 'c-footer'
+}
+</script>
